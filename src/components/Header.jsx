18b069792f841f5096ba25c9314ece05b8ad8ddd
Alexander Starykh <alexander000starykh@gmail.com>
2022-07-02 14:15:26 +0300

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (<nav className="deep-purple darken-2">
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">Shop On React</a>
+            <a href="#!" className="brand-logo">Shop On React</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="https://github.com/AlexanderStarykh/shop_on_react" target='_blank'>Repo</a></li>
             </ul>
